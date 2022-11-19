d3e617c617ec7c10c17e6e06ea226a96b151e160
GUchechi <godswilluchendu7@gmail.com>
2022-11-19 17:26:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
 
 function App() {
   return (
     <div className="App">
-    
+      {/* Particles js */}
+
+      {/* navBar */}
+
+      {/* Main Page Content */}
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route index path="/about" element={<About />} />
+        <Route index path="/resume" element={<Resume />} />
+        <Route index path="/skills" element={<Skills />} />
+        <Route index path="/portfolio" element={<Portfolio />} />
+        <Route index path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
