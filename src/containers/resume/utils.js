8b833d7b67ac1654ea3601b10f5a6a6b45cad559
GUchechi <godswilluchendu7@gmail.com>
2022-11-22 09:16:39 +0100

    portfolio

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,16 @@
+export const data = {
+  experience: [
+    {
+      title: "Front End Developer Intern",
+      subTitle: "WAAW FOUNDATION, ABUJA, NIGERIA",
+      description: "INTERNSHIP",
+    },
+  ],
+
+  education: [
+    {
+      title: "University Of Uyo, Nigeria",
+      description: "Bachelor of Arts in English",   
+    },
+  ],
+};
