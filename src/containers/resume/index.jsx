fb9f001055dd2f2aa6a5051d161ff84d394901b8
GUchechi <godswilluchendu7@gmail.com>
2022-11-22 09:35:26 +0100

    portfolio

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
         headerText="My Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
-      <div className="my_resume">
+      
       <div className="timeline">
         <div className="timeline__experience">
           <h3 className="timeline__experience__header-text">Experience</h3>
@@ -84,7 +84,6 @@ const Resume = () => {
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
-        </div>
         </div>
       </div>
     </section>
