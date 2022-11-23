250b500a96bcd90809e24fd790f3b9561348f82b
GUchechi <godswilluchendu7@gmail.com>
2022-11-24 00:20:10 +0100

    portfolio

---
+++ src/components/navBar/MenuList.js
@@ -0,0 +1,26 @@
+export const MenuList = [
+    {
+      label: "HOME",
+      to: "/",
+    },
+    {
+      label: "ABOUT ME",
+      to: "/about",
+    },
+    {
+      label: "SKILLS",
+      to: "/skills",
+    },
+    {
+      label: "PORTFOLIO",
+      to: "/portfolio",
+    },
+    {
+      label: "RESUME",
+      to: "/resume",
+    },
+    {
+      label: "CONTACT",
+      to: "/contact",
+    },
+  ];
